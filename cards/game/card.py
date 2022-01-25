71820e65335544a09adf0e9355451a22cbb499c1
Ethan Chipman <ethandchipman@gmail.com>
2022-01-24 19:06:05 -0700

    finished code

--- cards/game/card.py
+++ cards/game/card.py
@@ -23,6 +23,7 @@ class Game:
         self.score = 0
         self.total_score = 300
         self.old_card = 0
+        self.new_card = 0
         
         pass
 
@@ -37,31 +38,35 @@ class Game:
 
 
     def player_guess(self):
+
+        if not self.is_playing:
+            return
         
         card = Card()
-        card.card()
     
-        old_card = card.card()
-        new_card = card.card()
-        print(f'The card is: {old_card}')
-        card_guess = input('Higher or lower? (h/l): ')
-        print(f'Next card was: {new_card}')
+        self.old_card = card.card()
+        self.new_card = card.card()
+        print(f'The card is: {self.old_card}')
+        self.card_guess = input('Higher or lower? (h/l): ')
+        print(f'Next card was: {self.new_card}')
 
-        return card_guess
+        return self.card_guess
 
     def point_tally(self):
 
         if not self.is_playing:
             return
 
-        # self.score = 100 if self.old_card > self.new_card and self.player_guess == 'h'else 100 if self.old_card < self.new_card and self.player_guess == 'l' else -75
-        if self.old_card > self.new_card and self.player_guess == 'h':
-            self.score = 100
-        if self.old_card < self.new_card and self.player_guess == 'l':
-            self.score = 100
-        else:
-            self.score = -75
+        self.score = 100 if self.old_card > self.new_card and self.card_guess == 'l'else 100 if self.old_card < self.new_card and self.card_guess == 'h' else -75
+        #if self.old_card > self.new_card and self.card_guess == 'l':
+            #self.score = 100
+        #if self.old_card < self.new_card and self.card_guess == 'h':
+            #self.score = 100
+        #else:
+            #self.score = -75
         self.total_score += self.score
+
+        return self.total_score
         
     def output(self):
 
@@ -70,4 +75,7 @@ class Game:
 
         print(f'Your score is: {self.total_score}')
         again = input('Play again? (y/n): ')
-        self.is_playing == (self.total_score > 0 and again == 'y')
+        if self.total_score > 0 and again == 'y':
+            self.is_playing = True
+        else:
+            self.is_playing = False
